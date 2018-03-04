c443f89b0a17839fa23381267c3e96ffbf8a58b0
Magnus Jason <magnus@magnusjason.com>
2018-03-04 19:43:14 +1100

    Move bash script out of dockerfile. Provide a non-cgo build option for convenience.

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,9 @@
 FROM magj/go-ui-crossbuild-base
 WORKDIR /go/src
-RUN printf '#!/bin/sh\n\
-die () {\n\
-    echo >&2 "$@"\n\
-    exit 1\n\
-}\n\
-[ "$#" -eq 2 ] || die "Provide a project root path and a relative package path\\n\
-usage: github.com/name/project ./cmd/gui"\n\
-PROJECT_ROOT=$1\n\
-PACKAGE=$2\n\
-PACKAGE_BASENAME=`basename $PACKAGE| cut -f 1 -d '.'`\n\
-shift\n\
-shift\n\
-cd $PROJECT_ROOT\n\
-export CGO_ENABLED=1\n\
-export GOARCH=amd64\n\
-# Copy locally compiled libui to avoid linking error\n\
-# Remove when https://github.com/andlabs/ui/issues/230 fixed\n\
-cp /tmp/libui_linux_amd64.a vendor/github.com/andlabs/ui\n\
-echo "Building linux binary"\n\
-GOOS=linux CC=clang CXX=clang++ go build $PACKAGE $*\n\
-echo "Building windows binary"\n\
-GOOS=windows CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -ldflags "-H=windowsgui -extldflags=-s" $PACKAGE $*\n\
-echo "Building darwin binary"\n\
-GOOS=darwin CGO_LDFLAGS_ALLOW="-mmacosx-version-min.*" CC=o64-clang CXX=o64-clang++ go build -o ${PACKAGE_BASENAME}.app $PACKAGE $*\n\
-' >> /bin/gouicrossbuild
+COPY gouicrossbuild.sh /bin/gouicrossbuild
+RUN ln /bin/gouicrossbuild /bin/gocrossbuild
 RUN chmod +x /bin/gouicrossbuild
+RUN chmod +x /bin/gocrossbuild
 CMD printf 'Cross compiler executable image intended for use with the andlabs/ui go library\n\
 Builds binaries for amd64 linux/windows/darwin platforms\n\n\
 usage: Mount your project in to the image $GOPATH source directory(/go/src).\n\
---
+++ gouicrossbuild.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+die () {
+    printf >&2 "$@"
+    exit 1
+}
+if (( $# < 2 )); then die "Provide a project root path and a relative package path \nusage: github.com/name/project ./cmd/gui binary_output_path"; fi
+PROJECT_ROOT=$1
+PACKAGE=$2
+PACKAGE_BASENAME=`basename ${PACKAGE}| cut -f 1 -d '.'`
+if [ -n "$1" ]; then
+    mkdir -p $3
+    BUILD_OUTPUT="${3}/${PACKAGE_BASENAME}"
+else
+    BUILD_OUTPUT="${PACKAGE_BASENAME}"
+fi
+
+shift
+shift
+shift
+cd ${PROJECT_ROOT}
+export GOARCH=amd64
+
+# if not invoked via gouicrossbuild just do a simple go build
+if [ `basename $0` == "gouicrossbuild" ]; then
+    export CGO_ENABLED=1
+    # Copy locally compiled libui to avoid linking error
+    # Remove when https://github.com/andlabs/ui/issues/230 fixed
+    cp /tmp/libui_linux_amd64.a vendor/github.com/andlabs/ui
+    echo "Building linux binary"
+    GOOS=linux CC=clang CXX=clang++ go build -o ${BUILD_OUTPUT} ${PACKAGE} $*
+    echo "Building windows binary"
+    GOOS=windows CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o ${BUILD_OUTPUT}.exe -ldflags "-H=windowsgui -extldflags=-s" ${PACKAGE} $*
+    echo "Building darwin binary"
+    GOOS=darwin CGO_LDFLAGS_ALLOW="-mmacosx-version-min.*" CC=o64-clang CXX=o64-clang++ go build -o ${BUILD_OUTPUT}.app ${PACKAGE} $*
+else
+    echo "Building linux binary"
+    GOOS=linux go build -o ${BUILD_OUTPUT} ${PACKAGE} $*
+    echo "Building windows binary"
+    GOOS=windows go build -o ${BUILD_OUTPUT}.exe ${PACKAGE} $*
+    echo "Building darwin binary"
+    GOOS=darwin go build -o ${BUILD_OUTPUT} ${PACKAGE} $*
+fi
